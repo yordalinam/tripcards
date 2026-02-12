a03e0783dec405875575741b93155fad3a8b4870
yordalinam <yordalina.metodieva@gmail.com>
2026-02-12 14:23:33 +0200

    fix sorting -> rating to name

--- src/hooks/useFiltered.ts
+++ src/hooks/useFiltered.ts
@@ -16,13 +16,21 @@ export const useFilteredTrips = ({
 }: UseFilteredTripsProps) => {
   return useMemo(() => {
     const result = trips.filter((trip) =>
-      trip.name.toLowerCase().includes(searchQuery.toLowerCase())
+      trip.name.toLowerCase().includes(searchQuery.toLowerCase()) || trip.description.toLowerCase().includes(searchQuery.toLowerCase())
+
     );
 
     if (sortDirection) {
       result.sort((a, b) => {
-        const ratingDiff = b.rating - a.rating;
-        return sortDirection === "asc" ? -ratingDiff : ratingDiff;
+        const NameA = a.name.toLowerCase()
+        const NameB = b.name.toLowerCase()
+        if(NameA>NameB){
+          return sortDirection === 'asc' ? -1 : 1
+        }
+        if(NameB>NameA){
+          return sortDirection === 'asc' ? 1 : -1
+        }
+        return 0
       });
     }
     return result;
